d7d452dacd80db39588aa7d889728465affd1256
bobo <goujb@svocloud.com>
2019-04-05 16:35:50 +0800

    获取设备信息

--- notejs/public/js/mediaStream.js
+++ notejs/public/js/mediaStream.js
@@ -7,14 +7,41 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia()) {
     }
     navigator.mediaDevices.getUserMedia(constraints)
         .then(gotUserMedieStream)
+        .then(getMediaDevices)
         .catch(handleError);
 }
-var videlPlayer = document.querySelector("video#player")
+var audioInput = document.querySelector("select#audioInput")
+    , audioOutput = document.querySelector("select#audioOutput")
+    , videoInput = document.querySelector("select#videoInput")
+
+var videoPlayer = document.querySelector("video#player")
 
 function handleError(err) {
     console.log("error:" + err.name + ":" + err.message)
 }
 
 function gotUserMedieStream(stream) {
-    videlPlayer.srcObject = stream
+    videoPlayer.srcObject = stream
+    return navigator.mediaDevices.enumerateDevices()
+}
+
+function getMediaDevices(devicesInfos) {
+    devicesInfos.forEach(function (devicesInfo) {
+        console.log("kind=" + devicesInfo.kind +
+            ";label=" + devicesInfo.label +
+            ";id=" + devicesInfo.deviceId +
+            ";groupId:" + devicesInfo.groupId)
+        var option = document.createElement("option")
+        option.text = devicesInfo.label
+        option.value = devicesInfo.deviceId
+        if (devicesInfo.kind === 'audioinput') {
+            audioInput.appendChild(option)
+        } else if (devicesInfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option)
+        } else if (devicesInfo.kind === 'videoinput') {
+            videoInput.appendChild(option)
+
+        }
+    })
+
 }
