92f6d4f05aed0eca4d811d467bdc61d4044f63ab
bobo <goujb@svocloud.com>
2019-04-05 10:06:49 +0800

    获取设备信息

--- notejs/App.js
+++ notejs/App.js
@@ -1,7 +1,7 @@
 var http=require('http'),express = require('express'),serveIndex = require('serve-index')
 
 var app = express();
-// app.use(serveIndex('./public/'))
+app.use(serveIndex('./public/'))
 app.use(express.static('./public/'))
 
 http.createServer(app).listen(8081)
---
+++ notejs/public/js/getDevices.js
@@ -0,0 +1,17 @@
+if (!navigator.mediaDevices || navigator.mediaDevices.enumerateDevices()) {
+    console.log("浏览器不支持 mediaDevices 接口")
+} else {
+    navigator.mediaDevices.enumerateDevices()
+        .then()
+        .catch(handleError);
+}
+
+function handleError(err) {
+    console.log("error:" + err.name + ":" + err.message())
+}
+
+function getMediaDevices(devicesInfos){
+    devicesInfos.forEach(function (devicesInfo) {
+        console.log("kind="+devicesInfo.kind+";lable="+devicesInfo.label+";id="+devicesInfo.id+";groupId:"+devicesInfo.groupId)
+        })
+}
