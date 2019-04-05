59c53ad3dc19efeef0f4834fac0900f0d08a091e
bobo <goujb@svocloud.com>
2019-04-05 20:15:44 +0800

    音频获取

---
+++ notejs/public/js/audioStream.js
@@ -0,0 +1,78 @@
+let audioInput = document.querySelector("select#audioInput")
+    , audioOutput = document.querySelector("select#audioOutput")
+    , videoInput = document.querySelector("select#videoInput")
+    , filterSelect = document.querySelector("select#filter")
+    , snapshot = document.querySelector("button#snapshot")
+    , picture = document.querySelector("canvas#picture")
+    , audioPlayer = document.querySelector("audio#audioPlayer")
+var selectOver = false
+
+picture.width = 640;
+picture.height = 480;
+
+function start() {
+    let videoInputDeviceId = videoInput.value
+    let constraints = {
+        video: false,
+        audio: {
+            noiseSuppression: true
+        }
+
+    }
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia(constraints)) {
+        console.log("浏览器不支持 mediaDevices 接口")
+    } else {
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(gotUserMedieStream)
+            .then(getMediaDevices)
+            .catch(handleError);
+    }
+}
+
+
+function handleError(err) {
+    console.log("error:" + err.name + ":" + err.message)
+}
+
+function gotUserMedieStream(stream) {
+    audioPlayer.srcObject = stream
+    return navigator.mediaDevices.enumerateDevices()
+}
+
+
+function getMediaDevices(devicesInfos) {
+    if (selectOver) return
+    selectOver = true
+    devicesInfos.forEach(function (devicesInfo) {
+        console.log("kind=" + devicesInfo.kind +
+            ";label=" + devicesInfo.label +
+            ";deviceId=" + devicesInfo.deviceId +
+            ";groupId:" + devicesInfo.groupId)
+        let option = document.createElement("option")
+        option.text = devicesInfo.label
+        option.value = devicesInfo.deviceId
+        if (devicesInfo.kind === 'audioinput') {
+            audioInput.appendChild(option)
+        } else if (devicesInfo.kind === 'audiooutput') {
+            audioOutput.appendChild(option)
+        } else if (devicesInfo.kind === 'videoinput') {
+            videoInput.appendChild(option)
+        }
+    })
+
+}
+
+start();
+
+videoInput.onchange = start;
+
+// filterSelect.onchange = function () {
+//     videoPlayer.className = filterSelect.value
+// }
+// snapshot.onclick = function () {
+//     picture.getContext('2d').drawImage(videoPlayer
+//         , 0, 0
+//         , picture.width, picture.height
+//     )
+//     ;
+// }
